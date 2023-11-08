53b52c90dde3c05b8e5ad4055e17821b8808c278
Gaspar Tartari <gaspartartari@gmail.com>
2023-11-07 21:46:42 -0300

    Seller entitie class

--- src/application/App.java
+++ src/application/App.java
@@ -1,6 +1,9 @@
 package application;
 
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class App {
     public static void main(String[] args) throws Exception {
@@ -8,5 +11,8 @@ public class App {
         Department obj = new Department(1, "Books");
 
         System.out.println(obj.toString());
+
+        Seller seller = new Seller(23, "Bob", "bob@gmail.com", new Date(), 3000.0, obj);
+        System.out.println(seller.toString());
     }
 }
--- src/model/entities/Department.java
+++ src/model/entities/Department.java
@@ -3,7 +3,9 @@ package model.entities;
 import java.io.Serializable;
 
 public class Department implements Serializable {
+    
     private static final long serialVersionUID = 1L;
+
     private int id;
     private String name;
     
