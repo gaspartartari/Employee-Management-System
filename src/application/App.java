4a043be03f80ee7761b59a95ce7faf5dadc0c7cd
Gaspar Tartari <gaspartartari@gmail.com>
2023-11-08 14:16:03 -0300

    findById implementation

--- src/application/App.java
+++ src/application/App.java
@@ -10,13 +10,11 @@ import model.entities.Seller;
 public class App {
     public static void main(String[] args) throws Exception {
         
-        Department obj = new Department(1, "Books");
 
-        System.out.println(obj.toString());
+        SellerDao sellerDao = DaoFactory.createSellerDao();
 
-        Seller seller = new Seller(23, "Bob", "bob@gmail.com", new Date(), 3000.0, obj);
+        Seller seller = sellerDao.findById(3);
+
         System.out.println(seller.toString());
-
-        SellerDao sellerDao = DaoFactory.createSellerDown();
     }
 }
